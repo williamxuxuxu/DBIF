f1f0fe202ac08bf464cbf71627f96660d518e6c2
William Xu <williamxuxu@yahoo.com>
2017-03-05 03:31:03 -0800

    Rough code idea

--- player.cpp
+++ player.cpp
@@ -40,5 +40,9 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
      */
+
+// Use machine learning to figure out an ideal board metric such that a
+// strategy faithful to the metric yields the highest win rate.
+
     return nullptr;
 }
